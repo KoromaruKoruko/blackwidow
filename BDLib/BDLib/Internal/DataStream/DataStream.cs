c67798948ab6477237b3acce8e7818ab73cfc464
dedady157 <bailey.drahoss@outlook.com>
2017-08-03 13:22:22 +0200

    Update Fixed ...
    Overlapping StreamIDs
    moved StreamID into DataStreamInfo
    
    hopefully this makes it more stable and faster

--- BDLib/BDLib/Internal/DataStream/DataStream.cs
+++ BDLib/BDLib/Internal/DataStream/DataStream.cs
@@ -1,79 +1,89 @@
-﻿using System;
-using System.IO;
+using System;
 using System.Collections.Generic;
 
 namespace BDLib.Internal.DataStream
 {
     public static class DataStream
     {
-        private static List<UInt32> IDs;
         private static List<DataStreamInfo> Streams;
         private static Random IDGEN = new Random();
 
 
         public static UInt32 RegisterStream()
         {
-            byte[] ID = new byte[4];
-            IDGEN.NextBytes(ID);
-            UInt32 uint_ID = BitConverter.ToUInt32(ID, 0);
-            IDs.Add(uint_ID);
+            UInt32 uint_ID = GenID();
             Streams.Add(new DataStreamInfo()
             {
+                ID = uint_ID,
                 Data = new Queue<byte>(),
             });
             return uint_ID;
         }
+        private static UInt32 GenID()
+        {
+                UInt32 uint_ID = 0;
+                while (true)
+                {
+                    byte[] ID = new byte[4];
+                    IDGEN.NextBytes(ID);
+                    uint_ID = BitConverter.ToUInt32(ID, 0);
+                    SearchID = uint_ID;
+                    if (Streams.FindIndex(new Predicate<DataStreamInfo>(FindByID)) > 0) break;
+                }
+                return uint_ID;
+        }
+
         public static void UnRegisterStream(UInt32 ID)
         {
             SearchID = ID;
-            int x = IDs.FindIndex(new Predicate<uint>(FindByID));
-            IDs.RemoveAt(x);
+            int x = Streams.FindIndex(new Predicate<DataStreamInfo>(FindByID));
             Streams.RemoveAt(x);
         }
         
         public static bool Exists(UInt32 ID)
         {
             SearchID = ID;
-            return (IDs.FindIndex(new Predicate<UInt32>(FindByID)) > 0) ? true : false;
+            return (Streams.FindIndex(new Predicate<DataStreamInfo>(FindByID)) > 0) ? true : false;
         }
 
 
         public static byte ReadFrom(UInt32 StreamID)
         {
             SearchID = StreamID;
-            return Streams[IDs.FindIndex(new Predicate<uint>(FindByID))].Data.Dequeue();
+            return Streams.Find(new Predicate<DataStreamInfo>(FindByID)).Data.Dequeue();
         }
         public static void WriteTo(UInt32 StreamID,byte Data)
         {
             SearchID = StreamID;
-            Streams[IDs.FindIndex(new Predicate<uint>(FindByID))].Data.Enqueue(Data);
+            Streams.Find(new Predicate<DataStreamInfo>(FindByID)).Data.Enqueue(Data);
         }
         public static byte[] ReadFrom(UInt32 StreamID,int Amount)
         {
             SearchID = StreamID;
             byte[] output = new byte[Amount];
             for(int x = 0; x < Amount; x++)
-                output[x] = Streams[IDs.FindIndex(new Predicate<uint>(FindByID))].Data.Dequeue();
+                output[x] = Streams.Find(new Predicate<DataStreamInfo>(FindByID)).Data.Dequeue();
             return output;
         }
         public static void WriteTo(UInt32 StreamID, byte[] Data)
         {
             SearchID = StreamID;
             for(int x = 0; x < Data.Length; x++)
-                Streams[IDs.FindIndex(new Predicate<uint>(FindByID))].Data.Enqueue(Data[x]);
+                Streams.Find(new Predicate<DataStreamInfo>(FindByID)).Data.Enqueue(Data[x]);
         }
 
 
 
         private static UInt32 SearchID;
-        private static bool FindByID(UInt32 ID)
+        private static bool FindByID(DataStreamInfo STM)
         {
-            return (ID == SearchID) ? true : false;
+            return (STM.ID == SearchID) ? true : false;
         }
     }
 
     internal struct DataStreamInfo
     {
+        public UInt32 ID;
         internal Queue<byte> Data;
         public int Available { get { return Data.Count; } }
     }
