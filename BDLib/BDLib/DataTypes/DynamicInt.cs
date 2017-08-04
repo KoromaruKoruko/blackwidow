859883a104bd13fbba305e9e1a7b5747173c28c9
dedady157 <bailey.drahoss@outlook.com>
2017-08-04 02:21:55 +0200

    Update
    added ability to subtract and auto Negative detection

--- BDLib/BDLib/DataTypes/DynamicInt.cs
+++ BDLib/BDLib/DataTypes/DynamicInt.cs
@@ -14,7 +14,7 @@ namespace BDLib.DataTypes
         {
             if (SizeInBytes % 2 != 0)
                 throw new ArgumentException("Size must be divisable by 2");
-            if (SizeInBytes < 4)
+            if (SizeInBytes < 8)
                 throw new ArgumentException("Size must be grater than Int");
 
             Data = new byte[SizeInBytes];
@@ -24,7 +24,7 @@ namespace BDLib.DataTypes
         {
             if (PreData.Length % 2 != 0)
                 throw new ArgumentException("Size must be divisable by 2");
-            if (PreData.Length < 4)
+            if (PreData.Length < 8)
                 throw new ArgumentException("Size must be grater than Int");
 
             Data = PreData;
@@ -47,17 +47,15 @@ namespace BDLib.DataTypes
         //inner workings
         private byte[] ToME(byte[] x, bool isNeg)
         {
-            //x is 4
             byte[] f = new byte[Data.Length];
-            Array.Copy(x, 0, f, (isNeg) ? 0 : SignPos, 4);
+            Array.Copy(x, 0, f, (isNeg) ? 0 : SignPos, x.Length);
             return f;
         }
-        private DynamicInt ToMe(bool[] x)
+        private DynamicInt ToME(bool[] x)
         {
             byte[] f = new byte[(x.Length / 8)];
             (new BitArray(x)).CopyTo(f, 0);
             return new DynamicInt(f);
-
         }
 
         private DynamicInt add(byte[] x)
@@ -97,7 +95,7 @@ namespace BDLib.DataTypes
                             break;
 
                         case (3):
-                            output = false;
+                            output = true;
                             carry = true;
                             break;
 
@@ -110,17 +108,36 @@ namespace BDLib.DataTypes
             if (carry)
                 throw new OverflowException();
 
-            return ToMe(outputs);
+            return ToME(outputs);
         }
         private DynamicInt mins(byte[] x)
         {
-            throw new NotImplementedException("comming soon");
+            //throw new NotImplementedException("comming soon");
             bool[] outputs = new bool[Data.Length * 8];
-            bool carry = false;
 
-            for (int c = Data.Length-1; c > 0; c--)
+            //get OnesComplament
+            for (int c = 0; c < Data.Length; c++)
             {
-                for (int y = 7; y > 0; y--)
+                for (int y = 7; y > -1; y--)
+                {
+                    outputs[(c * 8) + y] = ((x[c] & (1 << y)) != 0) ? false : true;
+                }
+            }
+
+            //get TwosComplament
+
+            bool[] tmp = new bool[Data.Length * 8];
+            for(int c = 0; c < tmp.Length; c++)
+            {
+                    tmp[c] = (c == ((Data.Length * 8) / 2) + 1);
+            }
+            x = ToME(tmp).Data;
+            tmp = null;
+
+            bool carry = false;
+            for (int c = 0; c < Data.Length; c++)
+            {
+                for (int y = 7; y < -1; y++)
                 {
                     bool X_bit = (x[c] & (1 << y)) != 0;
                     bool MY_bit = ((Data[c] & (1 << y)) != 0);
@@ -139,33 +156,17 @@ namespace BDLib.DataTypes
                             break;
 
                         case (1):
-                            if (X_bit)
-                            {
-                                output = false;
-                                carry = true;
-                            }
-                            else if(MY_bit)
-                            {
-                                output = true;
-                                carry = false;
-                            }
-                            else if(carry && c<SignPos)
-                            {
-                                carry = false;
-                                output = true;
-                            }
-                            break;
-
-                        case (2):
-                            if (carry && MY_bit)
-                                output = false;
-                            else
-                                output = true;
+                            output = true;
                             carry = false;
                             break;
 
-                        case (3):
+                        case (2):
                             output = false;
+                            carry = true;
+                            break;
+
+                        case (3):
+                            output = true;
                             carry = true;
                             break;
 
@@ -175,10 +176,61 @@ namespace BDLib.DataTypes
                     outputs[(c * 8) + y] = output;
                 }
             }
-            if (carry)
-                throw new OverflowException();
-            
-            return ToMe(outputs);
+
+            x = ToME(outputs).Data;
+
+            //do math
+            carry = false;
+            for (int c = 0; c < Data.Length; c++)
+            {
+                for (int y = 7; y < -1; y++)
+                {
+                    bool X_bit = (x[c] & (1 << y)) != 0;
+                    bool MY_bit = ((Data[c] & (1 << y)) != 0);
+                    int f = 0;
+
+                    if (MY_bit) f++;
+                    if (X_bit) f++;
+                    if (carry) f++;
+                    bool output = false;
+
+                    switch (f)
+                    {
+                        case (0):
+                            output = false;
+                            carry = false;
+                            break;
+
+                        case (1):
+                            output = true;
+                            carry = false;
+                            break;
+
+                        case (2):
+                            output = false;
+                            carry = true;
+                            break;
+
+                        case (3):
+                            output = true;
+                            carry = true;
+                            break;
+
+                        default:
+                            throw new Exception("WTF happend??");
+                    }
+                    outputs[(c * 8) + y] = output;
+                }
+            }
+
+            //flip result
+
+            for (int c = 0; c < Data.Length*8; c++)
+            {
+                outputs[c] = (outputs[c]) ? false : true;
+            }
+
+            return ToME(outputs);
         }
 
         //overrides
@@ -207,19 +259,21 @@ namespace BDLib.DataTypes
             return a.mins(b.Data);
         }
 
-        public static DynamicInt operator +(DynamicInt a, Int64 b)
-        {
-            if (b < 0)
-                return a.mins(a.ToME(BitConverter.GetBytes(-b), true));
-            else
-                return a.add(a.ToME(BitConverter.GetBytes(b), false));
-        }
-        public static DynamicInt operator -(DynamicInt a, Int64 b)
+        public static DynamicInt operator +(DynamicInt a, int b)
         {
             if (b > 0)
-                return a.mins(a.ToME(BitConverter.GetBytes(-b), true));
-            else
                 return a.add(a.ToME(BitConverter.GetBytes(b), false));
+            else if (b < 0)
+                return a.mins(a.ToME(BitConverter.GetBytes((-b)), false));
+            else return a;
+        }
+        public static DynamicInt operator -(DynamicInt a, int b)
+        {
+            if (b > 0)
+                return a.mins(a.ToME(BitConverter.GetBytes(b), false));
+            else if (b < 0)
+                return a.add(a.ToME(BitConverter.GetBytes((-b)), false));
+            else return a;
         }
 
         public static bool       operator <(DynamicInt a, DynamicInt b)
